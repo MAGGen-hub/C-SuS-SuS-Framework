13c2bf8e5b3d8ffc75ee8c302518be7f4385265f
MAGGen-hub <isaykov2001@mail.ru>
2024-12-17 04:04:38 +0300

    Nill checking operator rework.

--- out/modules/cssc/NC.lua
+++ out/modules/cssc/NC.lua
@@ -1,41 +1,41 @@
 local sub,insert,pairs,setmetatable,t_swap=ENV(6,7,14,19,24)
 --nil check (nil forgiving operator feature)
-Control:load_lib"code.cssc.runtime"
-Control:load_lib"code.cssc.op_stack"
-local stx = [[O
+C:load_lib"code.cssc.runtime"
+C:load_lib"code.cssc.op_stack"
+local stx,phf,tb,check = [[O
 ?. ?: ?( ?{ ?[ ?" ?'
-]] --all posible operators in current version
-local phf=function()end
+]], --all posible operators in current version
+function()end, --using default placeholder is not a variant!
+Cdata.skip_tb,t_swap{7,3,10}
 
-local runtime_meta=setmetatable({},{__call=function()end,__newindex=function()end})
-local runtime_func=function(obj) return obj==nil and runtime_meta or obj end
+local runtime_meta,runtime_dual_meta=setmetatable({},{__call=function()end,__newindex=function()end}),
+{__index=function()return phf end}--TODO: TEMPORAL SOLUTION! REWORK!
 
-local runtime_dual_meta={__index=function()return phf end}--TODO: TEMPORAL SOLUTION! REWORK!
-local runtime_dual_func=function(obj) return obj==nil and runtime_dual_meta or setmetatable({},{__index=function(self,i)return obj[i] or phf end}) end
-Control.Runtime.build("nilF.dual",runtime_dual_func)
-Control.Runtime.build("nilF.basic",runtime_func)
-local tb = Control.Cdata.skip_tb
-local check=t_swap{7,3,10}
+local runtime_func,runtime_dual_func=function(obj) return obj==nil and runtime_meta or obj end,
+function(obj) return obj==nil and runtime_dual_meta or setmetatable({},{__index=function(self,i)return obj[i] or phf end}) end
 
-Control:load_lib"code.syntax_loader"(stx,{O=function(...)
+Runtime.build("nilF.dual",runtime_dual_func)
+Runtime.build("nilF.basic",runtime_func)
+
+C:load_lib"code.syntax_loader"(stx,{O=function(...)
     for k,v in pairs{...}do
-        Control.Operators[v]=function() --shadow operator
-            local tp = sub(v,2)
+        Operators[v]=function() --shadow operator
+            local tp,i,d = sub(v,2)
             --todo prew ":" check on calls/indexing
 
-            local i,d=Control.Cdata.tb_while(tb)
-            if not check[d[1]] then Control.error("Unexpected '?' after '%s'!",Control.Result[i])end--error check before
+            i,d=Cdata.tb_while(tb)
+            if not check[d[1]] then C.error("Unexpected '?' after '%s'!",Result[i])end--error check before
 
-            Control.Event.run(2,"?x",2,1)--send events to fin opts in OP_st
-            Control.Event.run("all","?x",2,1)
+            Event.run(2,"?x",2,1)--send events to fin opts in OP_st
+            Event.run("all","?x",2,1)
             if tp==":" then --dual operatiom -> index -> call
-                Control.Runtime.reg("__cssc__op_d_nc","nilF.dual")
-                Control.configure_operator({{" ",5},{"__cssc__op_d_nc",3}},Control.Cdata.opts["."][1],false,false,true)
+                Runtime.reg("__cssc__op_d_nc","nilF.dual")
+                Control.configure_operator({{" ",5},{"__cssc__op_d_nc",3}},Cdata.opts["."][1],false,false,true)
             else
-                Control.Runtime.reg("__cssc__op_nc","nilF.basic")
-                Control.configure_operator({{" ",5},{"__cssc__op_nc",3}},Control.Cdata.opts["."][1],false,false,true)
+                Runtime.reg("__cssc__op_nc","nilF.basic")
+                Control.configure_operator({{" ",5},{"__cssc__op_nc",3}},Cdata.opts["."][1],false,false,true)
             end
-            Control.Text.split_seq(nil,1)--del "?"
+            Text.split_seq(nil,1)--del "?"
         end
     end
 end})
--- src/modules/cssc/NC.lua
+++ src/modules/cssc/NC.lua
@@ -1,41 +1,41 @@
 local sub,insert,pairs,setmetatable,t_swap=ENV(__ENV_SUB__,__ENV_INSERT__,__ENV_PAIRS__,__ENV_SETMETATABLE__,__ENV_T_SWAP__)
 --nil check (nil forgiving operator feature)
-Control:load_lib"code.cssc.runtime"
-Control:load_lib"code.cssc.op_stack"
-local stx = [[O
+C:load_lib"code.cssc.runtime"
+C:load_lib"code.cssc.op_stack"
+local stx,phf,tb,check = [[O
 ?. ?: ?( ?{ ?[ ?" ?'
-]] --all posible operators in current version
-local phf=function()end
+]], --all posible operators in current version
+function()end, --using default placeholder is not a variant!
+Cdata.skip_tb,t_swap{__STRING__,__WORD__,__CLOSE_BREAKET__}
 
-local runtime_meta=setmetatable({},{__call=function()end,__newindex=function()end})
-local runtime_func=function(obj) return obj==nil and runtime_meta or obj end
+local runtime_meta,runtime_dual_meta=setmetatable({},{__call=function()end,__newindex=function()end}),
+{__index=function()return phf end}--TODO: TEMPORAL SOLUTION! REWORK!
 
-local runtime_dual_meta={__index=function()return phf end}--TODO: TEMPORAL SOLUTION! REWORK!
-local runtime_dual_func=function(obj) return obj==nil and runtime_dual_meta or setmetatable({},{__index=function(self,i)return obj[i] or phf end}) end
-Control.Runtime.build("nilF.dual",runtime_dual_func)
-Control.Runtime.build("nilF.basic",runtime_func)
-local tb = Control.Cdata.skip_tb
-local check=t_swap{__STRING__,__WORD__,__CLOSE_BREAKET__}
+local runtime_func,runtime_dual_func=function(obj) return obj==nil and runtime_meta or obj end,
+function(obj) return obj==nil and runtime_dual_meta or setmetatable({},{__index=function(self,i)return obj[i] or phf end}) end
 
-Control:load_lib"code.syntax_loader"(stx,{O=function(...)
+Runtime.build("nilF.dual",runtime_dual_func)
+Runtime.build("nilF.basic",runtime_func)
+
+C:load_lib"code.syntax_loader"(stx,{O=function(...)
     for k,v in pairs{...}do
-        Control.Operators[v]=function() --shadow operator
-            local tp = sub(v,2)
+        Operators[v]=function() --shadow operator
+            local tp,i,d = sub(v,2)
             --todo prew ":" check on calls/indexing
 
-            local i,d=Control.Cdata.tb_while(tb)
-            if not check[d[1]] then Control.error("Unexpected '?' after '%s'!",Control.Result[i])end--error check before
+            i,d=Cdata.tb_while(tb)
+            if not check[d[1]] then C.error("Unexpected '?' after '%s'!",Result[i])end--error check before
 
-            Control.Event.run(__OPERATOR__,"?x",__OPERATOR__,__TRUE__)--send events to fin opts in OP_st
-            Control.Event.run("all","?x",__OPERATOR__,__TRUE__)
+            Event.run(__OPERATOR__,"?x",__OPERATOR__,__TRUE__)--send events to fin opts in OP_st
+            Event.run("all","?x",__OPERATOR__,__TRUE__)
             if tp==":" then --dual operatiom -> index -> call
-                Control.Runtime.reg("__cssc__op_d_nc","nilF.dual")
-                Control.configure_operator({{" ",__SPACE__},{"__cssc__op_d_nc",__WORD__}},Control.Cdata.opts["."][1],false,false,true)
+                Runtime.reg("__cssc__op_d_nc","nilF.dual")
+                Control.configure_operator({{" ",__SPACE__},{"__cssc__op_d_nc",__WORD__}},Cdata.opts["."][1],false,false,true)
             else
-                Control.Runtime.reg("__cssc__op_nc","nilF.basic")
-                Control.configure_operator({{" ",__SPACE__},{"__cssc__op_nc",__WORD__}},Control.Cdata.opts["."][1],false,false,true)
+                Runtime.reg("__cssc__op_nc","nilF.basic")
+                Control.configure_operator({{" ",__SPACE__},{"__cssc__op_nc",__WORD__}},Cdata.opts["."][1],false,false,true)
             end
-            Control.Text.split_seq(nil,1)--del "?"
+            Text.split_seq(nil,1)--del "?"
         end
     end
 end})
