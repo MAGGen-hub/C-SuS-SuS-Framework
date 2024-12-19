677c530aa9d7bdaa888d49f789809b495ed8adc6
MAGGen-hub <isaykov2001@mail.ru>
2024-12-19 21:54:02 +0300

    Keyword shortcuts rework

--- out/modules/cssc/KS.lua
+++ out/modules/cssc/KS.lua
@@ -1,14 +1,13 @@
 local match,sub,insert,pairs=ENV(2,6,7,14)
 
-local mod,arg=... --keyword shorcuts
---require"cc.pretty".pretty_print(arg)
-local stx = [[O
+local stx,_,arg = [[O
 || or
 && and
 ! not
 @ local
 $ return
-]]
+]],...
+
 for _,v in pairs(arg or{})do
 	if v=="sc_end" then --include semicolon to end conversion basic ; can be placed with \;
 		stx=stx.."; end\n\\; ;\n"
@@ -23,19 +22,16 @@ for _,v in pairs(arg or{})do
 end
 --specific make react with space addition
 local make_react=function(s,i,j) -- s -> replacer string, i - type of reaction, t - type of sequnece, j - local length
-	return function(Control)
-		--Control.Result[#Control.Result]= Control.Result[#Control.Result].." "
+	return function()
 		Control.inject(nil," ",5)
-		
-		Control.operator=sub(Control.operator,j+1)
-		Control.index=Control.index+j
-		insert(Control.Result,s)
-		
-		Control.Core(i,s)
+		C.operator=sub(C.operator,j+1)
+		C.index=C.index+j
+		insert(Result,s)
+		Core(i,s)
 		Control.inject(nil," ",5)
 	end
 end
-Control:load_lib"code.syntax_loader"(stx,{O=function(k,v)
-	Control.Operators[k]=make_react(v,match(v,"^[aon]") and 2 or 4,#k)
+C:load_lib"code.syntax_loader"(stx,{O=function(k,v)
+	Operators[k]=make_react(v,match(v,"^[aon]") and 2 or 4,#k)
 end})
 --return 1
--- src/modules/cssc/KS.lua
+++ src/modules/cssc/KS.lua
@@ -1,14 +1,13 @@
 local match,sub,insert,pairs=ENV(__ENV_MATCH__,__ENV_SUB__,__ENV_INSERT__,__ENV_PAIRS__)
 
-local mod,arg=... --keyword shorcuts
---require"cc.pretty".pretty_print(arg)
-local stx = [[O
+local stx,_,arg = [[O
 || or
 && and
 ! not
 @ local
 $ return
-]]
+]],...
+
 for _,v in pairs(arg or{})do
 	if v=="sc_end" then --include semicolon to end conversion basic ; can be placed with \;
 		stx=stx.."; end\n\\; ;\n"
@@ -23,19 +22,16 @@ for _,v in pairs(arg or{})do
 end
 --specific make react with space addition
 local make_react=function(s,i,j) -- s -> replacer string, i - type of reaction, t - type of sequnece, j - local length
-	return function(Control)
-		--Control.Result[#Control.Result]= Control.Result[#Control.Result].." "
+	return function()
 		Control.inject(nil," ",__SPACE__)
-		
-		Control.operator=sub(Control.operator,j+1)
-		Control.index=Control.index+j
-		insert(Control.Result,s)
-		
-		Control.Core(i,s)
+		C.operator=sub(C.operator,j+1)
+		C.index=C.index+j
+		insert(Result,s)
+		Core(i,s)
 		Control.inject(nil," ",__SPACE__)
 	end
 end
-Control:load_lib"code.syntax_loader"(stx,{O=function(k,v)
-	Control.Operators[k]=make_react(v,match(v,"^[aon]") and __OPERATOR__ or __KEYWORD__,#k)
+C:load_lib"code.syntax_loader"(stx,{O=function(k,v)
+	Operators[k]=make_react(v,match(v,"^[aon]") and __OPERATOR__ or __KEYWORD__,#k)
 end})
 --return __TRUE__
