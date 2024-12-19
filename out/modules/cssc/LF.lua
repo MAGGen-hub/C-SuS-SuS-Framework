2b181b64332d4b37ad7773c1254bf0a0f109326b
MAGGen-hub <isaykov2001@mail.ru>
2024-12-19 21:47:32 +0300

    Lambda function rework

--- out/modules/cssc/LF.lua
+++ out/modules/cssc/LF.lua
@@ -1,38 +1,33 @@
-local match,sub=ENV(2,6)
+local tb,fk,match,sub=Cdata.skip_tb,"function",ENV(2,6)
 
-local tb,fk = Control.Cdata.skip_tb,"function"--mk hash table
-Control.Operators["->"]=function(Control)
-	local s,ei,ed,cor,br=3,Control.Cdata.tb_while(tb)--get last esenshual_index,esenshual_data
-	if match(Control.Result[ei],"^%)")then--breaket located
+Operators["->"]=function()
+	local s,ei,ed,cor,br=3,Cdata.tb_while(tb)--get last esenshual_index,esenshual_data
+	if match(Result[ei],"^%)")then--breaket located
 		ei=ed[2]
-		--Control.log("EI:%d - %s;%s;%s;",ei,Control.Result[ei],match(Control.Result[ei],"^[=%(,]"), ei and match(Control.Result[ei],"^[=%(,]"))
-		cor = ei and match(Control.Result[Control.Cdata.tb_while(tb,ei-1)]or"","^[=%(,]")--coma is acceptable here 
-		--Control.log("COR:%s",cor)
-	else--default args
-		while ei>0 and(tb[ed[1]] or ed[1]==s or s~=3 and ((ed[2]or-1)==Control.Cdata.opts[","][1] and match(Control.Result[ei],"^%,")))do
+		cor = ei and match(Result[Cdata.tb_while(tb,ei-1)]or"","^[=%(,]")--coma is acceptable here 
+	else--breaketless args, probably will be removed in future
+		while ei>0 and(tb[ed[1]] or ed[1]==s or s~=3 and ((ed[2]or-1)==Cdata.opts[","][1] and match(Result[ei],"^%,")))do
 			ei,s=ei-1,s*(tb[ed[1]] and-1 or 1)--com skip/swap state 3/2(coma)
-			ed=Control.Cdata[ei]
+			ed=Cdata[ei]
 		end
-		ei,br,cor=ei+1,1,ei>0 and s~=3 and match(Control.Result[ei],"^[=%(]")
+		ei,br,cor=ei+1,1,ei>0 and s~=3 and match(Result[ei],"^[=%(]")
 	end
-	if not cor then Control.error("Corrupted lambda arguments at line %d !",Control.line)Control.Text.split_seq(nil,2) return end
+	if not cor then Control.error("Corrupted lambda arguments at line %d !",C.line)Text.split_seq(nil,2) return end
 	
-	--Control.inject(ei,"" @@DEBUG .."--[[cl mrk]]"
-	--,2,Control.Cdata.opts[":"][1])--call mark
 	Control.inject(nil," ",5)
 	Control.inject(ei,fk,4)--inject function kwrd
 	if br then --place breakets
 		Control.inject(ei+1,"(",9)--inject open breaket
 		Control.inject(nil,")",10,ei+1)--inject closeing breaket
 	end
-	if"-"==sub(Control.operator,1,1)then Control.inject(nil,"return",4) Control.inject(nil," ",5) end--inject return kwrd
+	if"-"==sub(C.operator,1,1)then Control.inject(nil,"return",4) Control.inject(nil," ",5) end--inject return kwrd
 
-	Control.Event.run(2,"->",2,1)--Iinform core about insertet operators (1 means cssc_generated)
-	Control.Event.run("all",sub(Control.operator,1,1)..">",tp,1)
+	Event.run(2,"->",2,1)--Iinform core about insertet operators (1 means cssc_generated)
+	Event.run("all",sub(C.operator,1,1)..">",tp,1)
 
-	Control.Level.open(fk,nil,ei)--open new function level (auto end set)
-	Control.Level[#Control.Level].DA_np=nil --prevent DEF ARGS
-	Control.Text.split_seq(nil,2)-- remove ->/=> from Control.operator
+	Level.open(fk,nil,ei)--open new function level (auto end set)
+	Level[#Level].DA_np=nil --prevent DEF ARGS
+	Text.split_seq(nil,2)-- remove ->/=> from Control.operator
 end
-Control.Operators["=>"]=Control.Operators["->"]
+Operators["=>"]=Operators["->"]
 --return 1
--- src/modules/cssc/LF.lua
+++ src/modules/cssc/LF.lua
@@ -1,38 +1,33 @@
-local match,sub=ENV(__ENV_MATCH__,__ENV_SUB__)
+local tb,fk,match,sub=Cdata.skip_tb,"function",ENV(__ENV_MATCH__,__ENV_SUB__)
 
-local tb,fk = Control.Cdata.skip_tb,"function"--mk hash table
-Control.Operators["->"]=function(Control)
-	local s,ei,ed,cor,br=__WORD__,Control.Cdata.tb_while(tb)--get last esenshual_index,esenshual_data
-	if match(Control.Result[ei],"^%)")then--breaket located
+Operators["->"]=function()
+	local s,ei,ed,cor,br=__WORD__,Cdata.tb_while(tb)--get last esenshual_index,esenshual_data
+	if match(Result[ei],"^%)")then--breaket located
 		ei=ed[2]
-		--Control.log("EI:%d - %s;%s;%s;",ei,Control.Result[ei],match(Control.Result[ei],"^[=%(,]"), ei and match(Control.Result[ei],"^[=%(,]"))
-		cor = ei and match(Control.Result[Control.Cdata.tb_while(tb,ei-1)]or"","^[=%(,]")--coma is acceptable here 
-		--Control.log("COR:%s",cor)
-	else--default args
-		while ei>0 and(tb[ed[1]] or ed[1]==s or s~=__WORD__ and ((ed[2]or-1)==Control.Cdata.opts[","][1] and match(Control.Result[ei],"^%,")))do
+		cor = ei and match(Result[Cdata.tb_while(tb,ei-1)]or"","^[=%(,]")--coma is acceptable here 
+	else--breaketless args, probably will be removed in future
+		while ei>0 and(tb[ed[1]] or ed[1]==s or s~=__WORD__ and ((ed[2]or-1)==Cdata.opts[","][1] and match(Result[ei],"^%,")))do
 			ei,s=ei-1,s*(tb[ed[1]] and-1 or 1)--com skip/swap state __WORD__/__OPERATOR__(coma)
-			ed=Control.Cdata[ei]
+			ed=Cdata[ei]
 		end
-		ei,br,cor=ei+1,__TRUE__,ei>0 and s~=__WORD__ and match(Control.Result[ei],"^[=%(]")
+		ei,br,cor=ei+1,__TRUE__,ei>0 and s~=__WORD__ and match(Result[ei],"^[=%(]")
 	end
-	if not cor then Control.error("Corrupted lambda arguments at line %d !",Control.line)Control.Text.split_seq(nil,2) return end
+	if not cor then Control.error("Corrupted lambda arguments at line %d !",C.line)Text.split_seq(nil,2) return end
 	
-	--Control.inject(ei,"" @@DEBUG .."--[[cl mrk]]"
-	--,__OPERATOR__,Control.Cdata.opts[":"][1])--call mark
 	Control.inject(nil," ",__SPACE__)
 	Control.inject(ei,fk,__KEYWORD__)--inject function kwrd
 	if br then --place breakets
 		Control.inject(ei+1,"(",__OPEN_BREAKET__)--inject open breaket
 		Control.inject(nil,")",__CLOSE_BREAKET__,ei+1)--inject closeing breaket
 	end
-	if"-"==sub(Control.operator,1,1)then Control.inject(nil,"return",__KEYWORD__) Control.inject(nil," ",__SPACE__) end--inject return kwrd
+	if"-"==sub(C.operator,1,1)then Control.inject(nil,"return",__KEYWORD__) Control.inject(nil," ",__SPACE__) end--inject return kwrd
 
-	Control.Event.run(__OPERATOR__,"->",__OPERATOR__,__TRUE__)--Iinform core about insertet operators (__TRUE__ means cssc_generated)
-	Control.Event.run("all",sub(Control.operator,1,1)..">",tp,__TRUE__)
+	Event.run(__OPERATOR__,"->",__OPERATOR__,__TRUE__)--Iinform core about insertet operators (__TRUE__ means cssc_generated)
+	Event.run("all",sub(C.operator,1,1)..">",tp,__TRUE__)
 
-	Control.Level.open(fk,nil,ei)--open new function level (auto end set)
-	Control.Level[#Control.Level].DA_np=nil --prevent DEF ARGS
-	Control.Text.split_seq(nil,2)-- remove ->/=> from Control.operator
+	Level.open(fk,nil,ei)--open new function level (auto end set)
+	Level[#Level].DA_np=nil --prevent DEF ARGS
+	Text.split_seq(nil,2)-- remove ->/=> from Control.operator
 end
-Control.Operators["=>"]=Control.Operators["->"]
+Operators["=>"]=Operators["->"]
 --return __TRUE__
